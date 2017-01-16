b36da59b245244d46b89152838ff7b960c9dcb60
vmitov <venelin.mitov@bsse.ethz.ch>
2017-01-16 15:18:17 +0100

    Added plotting methods. Implemented but commented call to gpuR types in the likelihood calculation. Currently this feature is not supported but can be interesting once the gpuR types support subsetting. Added logLik.POUMM and coef.POUMM methods. Added the fitted.POUMM method which does inference of the genotypic values at the tips. Implemented the specifyPOUMM function which validates makes a consistency check of various parameters of the POUMM method.

---
+++ R/paramsPOUMM.R
@@ -0,0 +1,319 @@
+#' @rdname PhylogeneticH2
+#' @title Phylogenetic Heritability
+#'   
+#' @description The phylogenetic heritability, \eqn{H^2}, is defined as the 
+#'   ratio of the genetic variance over the total phenotypic variance expected 
+#'   at a given evolutionary time t (measured from the root of the tree). Thus,
+#'   the phylogenetic heritability connects the parameters alpha, sigma and
+#'   sigmae of the POUMM model through a set of equations. The functions
+#'   described here provide an R-implementation of these equations.
+#'   
+#' @param t Numeric value denoting evolutionary time (i.e. distance from the 
+#'   root of a phylogenetic tree).
+#' @param alpha,sigma Numeric values or n-vectors, parameters of the OU process;
+#'   alpha and sigma must be non-negative. A zero alpha is interpreted as the 
+#'   Brownian motion process in the limit alpha -> 0.
+#' @param sigmae Numeric, environmental phenotypic deviation at the tips.
+#' @param H2 Phylogenetic heritability at time t.
+#' @param z Numerical vector of observed phenotypes.
+#' @param tree A phylo object.
+#' @param tFrom,tTo Numerical minimal and maximal root-tip distance to limit the
+#'   calculation.
+#'   
+#' @return All functions return numerical values or NA, in case of invalid 
+#'   parameters
+#' @examples 
+#' # At POUMM stationary state (equilibrium, t=Inf)
+#' H2 <- H2(alpha = 0.75, sigma = 1, sigmae = 1, t = Inf)     # 0.4
+#' alpha <- alpha(H2 = H2, sigma = 1, sigmae = 1, t = Inf)    # 0.75
+#' sigma <- sigma(H2 = H2, alpha = 0.75, sigmae = 1, t = Inf) # 1
+#' sigmae <- sigmae(H2 = H2, alpha = 0.75, sigma = 1, t = Inf) # 1
+#' 
+#' # At finite time t = 0.2
+#' H2 <- H2(alpha = 0.75, sigma = 1, sigmae = 1, t = 0.2)     # 0.1473309
+#' alpha <- alpha(H2 = H2, sigma = 1, sigmae = 1, t = 0.2)    # 0.75
+#' sigma <- sigma(H2 = H2, alpha = 0.75, sigmae = 1, t = 0.2) # 1
+#' sigmae <- sigmae(H2  =  H2, alpha = 0.75, sigma = 1, t = 0.2) # 1
+#' 
+#' # Comparing with the empirical H2e from a simulation
+#' N <- 20 
+#' tree <- TreeSim::sim.bd.taxa(N, 1, lambda = 2, mu = 1, complete =
+#' FALSE)[[1]] 
+#' tMean <- mean(nodeTimes(tree, tipsOnly = TRUE))
+#' z <- rVNodesGivenTreePOUMM(tree, 8, 2, 2, 1)
+#'   
+#' phytools::plotBranchbyTrait(tree, z, mode = "nodes", show.tip.label =
+#'   FALSE, show.node.label = FALSE) 
+#' ape::nodelabels(round(z[-(1:N)], 2)) 
+#' ape::tiplabels(round(z[(1:N)], 2))
+#'    
+#' @name PhylogeneticH2
+#' @seealso OU
+NULL
+
+#' @describeIn PhylogeneticH2 Calculate alpha given time t, H2, sigma and sigmae
+#' 
+#' @details The function alpha invokes the gsl function lambert_W0.
+#' 
+#' @importFrom gsl lambert_W0
+#'
+#' @export
+alpha <- function(H2, sigma, sigmae, t = Inf) {
+  if(!(H2>=0 & H2 <=1 & sigma >= 0 & sigmae >=0 & t>=0)) {
+    warning(paste0("Function poumm::alpha was called on invalid parameters. ", 
+                   "Check that H2>=0 & H2 <=1 & sigma >= 0 & sigmae >=0 & t>=0.",
+                   "Parameter values were: ", 
+                   toString(c(H2=H2, sigma=sigma, sigmae=sigmae, t=t))))
+    NA
+  }
+  if(is.infinite(t)) {
+    if(H2 == 1) {
+      # Assume correctly defined PMM, i.e. Brownian motion with sigma > 0 and 
+      # environmental deviation with sigmae >= 0
+      0
+    } else {
+      # H2 is the phylogenetic heritability at equilibrium
+      sigma^2 * (1 - H2) / (2 * H2 * sigmae^2)    
+    }
+  } else {
+    # finite t
+    y <- sigma^2 / sigmae^2 * (1 / H2 - 1)
+    if(is.na(y) | is.infinite(y)) {
+      as.double(NA)
+    } else {
+      (t * y + lambert_W0((-exp(-t * y)) * t * y)) / (2 * t)
+    }
+  }
+}
+
+#' @describeIn PhylogeneticH2 Calculate sigma given time t, H2 at time t, alpha
+#'   and sigmae
+#'   
+#' @export
+sigma <- function(H2, alpha, sigmae, t=Inf) {
+  if(H2>1 | H2<0 | alpha < 0 | sigmae < 0 | t < 0) {
+    stop("H2(t) should be in [0, 1], alpha, sigmae and t should be non-negative.")
+  }
+  res <- if(is.infinite(t)) {
+    if(alpha == 0) {
+      # BM
+      sqrt(sigmae^2 * H2 / (t * (1 - H2)))
+    } else {
+      # alpha>0, OU in equilibrium
+      sqrt(2 * alpha * H2 * sigmae^2 / (1 - H2))
+    }
+  } else {
+    # t is finite
+    if(alpha == 0) {
+      # BM
+      sqrt(sigmae^2 * H2 / (t * (1 - H2)))
+    } else {
+      # alpha>0, OU not in equilibrium
+      sqrt(2 * alpha * H2 * sigmae^2 / ((1 - exp(-2 * alpha * t)) * (1 - H2)))
+    } 
+  }
+  names(res) <- NULL
+  res
+}
+
+#' @describeIn PhylogeneticH2 Calculate sigmae given alpha, sigma, and H2 at
+#'   time t
+#' @details The function sigmae uses the formula H2 = varOU(t, alpha, sigma) /
+#'   (varOU(t, alpha, sigma) + sigmae^2)
+#'   
+#' @export
+sigmae <- function(H2, alpha, sigma, t = Inf) {
+  sigmaG2 <- varOU(t, alpha, sigma)
+  sqrt(sigmaG2 / H2 - sigmaG2)
+}
+
+#' @describeIn PhylogeneticH2 "Empirical" phylogenetic heritability estimated
+#'   from the empirical variance of the observed phenotypes and sigmae
+#'   
+#' @importFrom stats var
+#' @export
+H2e <- function(z, sigmae, tree=NULL, tFrom=0, tTo=Inf) {
+  if(!is.null(tree)) {
+    tipTimes <- nodeTimes(tree)[1:length(tree$tip.label)]
+    z <- z[which(tipTimes >= tFrom & tipTimes <= tTo)]
+  }
+  1 - (sigmae^2 / var(z))
+}
+
+#' Phylogenetic heritability estimated at time t
+#' @param alpha,sigma numeric, parameters of the OU process acting on the
+#'   genetic contributions
+#' @param sigmae numeric, environmental standard deviation
+#' @param t time from the beginning of the process at which heritability should
+#'   be calculated, i.e. epidemiologic time
+#' @param tm average time for within host evolution from getting infected until
+#'   getting measured or passing on the infection to another host
+#' @export
+H2 <- function(alpha, sigma, sigmae, t = Inf, tm = 0) {
+  lenoutput <- max(sapply(list(alpha, sigma, sigmae, t, tm), length))
+  if(lenoutput>1) {
+    alpha <- rep(alpha, lenoutput / length(alpha))
+    sigma <- rep(sigma, lenoutput / length(sigma))
+    sigmae <- rep(sigmae, lenoutput / length(sigmae))
+    t <- rep(t, lenoutput / length(t))
+    tm <- rep(tm, lenoutput / length(tm))
+  }
+  sigmag2 <- ifelse(alpha > 0, 
+                    0.5*(1 - exp(-2 * alpha * t)) / alpha * sigma^2, 
+                    t * sigma^2)  
+  sigmagm2 <- ifelse(alpha > 0, 
+                     0.5*(1 - exp(-2 * alpha * tm)) / alpha * sigma^2, 
+                     tm * sigma^2)
+  
+  values <- ifelse(t > tm, 
+                   (sigmag2 - sigmagm2) / (sigmag2 + sigmae^2), 
+                   rep(0, length(t)))
+  
+  values <- ifelse(alpha == 0 & sigma > 0 & is.infinite(t) & !is.infinite(sigmae),
+                   1, values)
+  values
+}
+
+#' Expected covariance of two tips at given root-tip time and phylogenetic distance
+#' 
+#' @param alpha,sigma,sigmae POUMM parameters
+#' @param t A non-negative number or vector time from the beginning of the POUMM
+#'   process (root-tip distance). If a vector, the evaluation is done on each 
+#'   couple (row) from cbind(t, tau).
+#' @param tau A non-negative number or vector indicating the phylogenetic 
+#'   distance between two tips, each of them located at time t from the root. 
+#'   If a vector, the evaluation is done on each couple (row) from cbind(t, tau).
+#' @param corr Logical indicating whether correlation should be returned instead
+#' of covariance.
+#' @param as.matrix Logical indicating if a variance-covariance matrix should be
+#' returned.
+#' 
+#' @details The function assumes that the two tips are at equal distance t from 
+#' the root. This implies that the root-tip distance of their mrca is t - tau/2.
+#' 
+#' @return If as.matrix == FALSE, a number. Otherwise a two by two symmetric
+#'   matrix. If t or tau is a vector of length bigger than 1, then a vector of 
+#'   numbers or a list of matrices.
+#' 
+#' @export
+covPOUMM <- function(alpha, sigma, sigmae, t, tau, corr = FALSE, as.matrix = FALSE) {
+  if(length(t) == 1 & length(tau) == 1) {
+    covMat <- covVTipsGivenTreePOUMM(
+      tree = NULL, alpha = alpha, sigma = sigma, sigmae = sigmae,
+      tanc = rbind(c(t, t-tau/2), c(t-tau/2, t)), 
+      tau = rbind(c(0, tau), c(tau, 0)), corr = corr)
+    if(as.matrix) {
+      covMat
+    } else {
+      covMat[1,2]
+    }
+  } else {
+    ttau <- cbind(t, tau)
+    apply(ttau, 1, function(.) {
+      t <- .[1]
+      tau <- .[2]
+      covMat <- covVTipsGivenTreePOUMM(
+        tree = NULL, alpha = alpha, sigma = sigma, sigmae = sigmae,
+        tanc = rbind(c(t, t-tau/2), c(t-tau/2, t)), 
+        tau = rbind(c(0, tau), c(tau, 0)), corr = corr)
+      if(as.matrix) {
+        covMat
+      } else {
+        covMat[1,2]
+      }
+    })
+  }
+}
+
+#' Variance covariance matrix of the values at the tips of a tree under an OU 
+#' process
+#' 
+#' @param tree A phylo object.
+#' @param alpha,sigma Non-negative numeric values, parameters of the OU process.
+#' @param sigmae Non-negative numeric value, environmental standard deviation at
+#'   the tips. 
+#' @param corr Logical indicating if a correlation matrix shall be returned.
+#' @param tanc Numerical matrix with the time-distance from the root of the tree
+#'   to the mrca of each tip-couple. If NULL it will be calculated.
+#' @param tauij Numerical matrix with the time (patristic) distance between each pair of tips. 
+#'   If NULL, it will be calculated. 
+#' @return a variance covariance or a correlation matrix of the tips in tree.
+#' @references (Hansen 1997) Stabilizing selection and the comparative analysis 
+#'   of adaptation.
+#'   
+#' @export
+covVTipsGivenTreePOUMM <- function(
+  tree, alpha = 0, sigma = 1, sigmae = 0, tanc = NULL, tauij = NULL, corr = FALSE) {
+  
+  # distances from the root to the mrca's of each tip-couple.
+  if(is.null(tanc)) {
+    tanc <- ape::vcv(tree)
+  } else {
+    tanc <- as.matrix(tanc)
+  }
+  
+  N <- dim(tanc)[1]
+  if(alpha > 0) {
+    varanc <- 0.5 * (1 - exp(-2 * alpha * tanc)) / alpha
+  } else {
+    # limit case alpha==0
+    varanc <- tanc
+  }
+  
+  # time from the root to each tip
+  ttips <- diag(tanc) 
+  
+  # distance-matrix between tips
+  if(is.null(tauij)) {
+    tauij <- sapply(ttips, function(.) . + ttips) - 2 * tanc  
+  } else {
+    tauij <- as.matrix(tauij)
+  }
+  
+  covij <- exp(-alpha * tauij) * varanc * sigma^2 + diag(x = sigmae^2, nrow = N)
+  if(corr) {
+    sdi <- sqrt(diag(covij))
+    sdi_sdj <- sapply(sdi, function(.) . * sdi)
+    covij / sdi_sdj
+  } else {
+    covij
+  }  
+}
+
+
+
+#' Distribution of the genotypic values under a POUMM fit
+#' 
+#' @param tree an object of class phylo
+#' @param z A numeric vector of size length(tree$tip.label) representing the trait
+#'     values at the tip-nodes.
+#' @param g0 A numeric value at the root of the tree, genotypic contribution.
+#' @param alpha,theta,sigma Numeric values, parameters of the OU model.
+#' @param sigmae Numeric non-negative value (default 0). Specifies the standard 
+#'   deviation of random environmental contribution (white noise) included in z.
+gPOUMM <- function(z, tree, g0, alpha, theta, sigma, sigmae) {
+  N <- length(tree$tip.label)
+  
+  #V.g <- cov.poumm(tree, alpha, sigma, sigmae) ###???? shouldn't sigmae be 0 here?
+  V.g <- covVTipsGivenTreePOUMM(tree, alpha, sigma)
+  V.g_1 <- chol2inv(chol(V.g))
+  mu.g <- meanOU(g0, t = nodeTimes(tree, tipsOnly = TRUE), alpha = alpha, theta = theta)
+  
+  V.e <- diag(sigmae^2, nrow=N, ncol=N)
+  V.e_1 <- V.e
+  diag(V.e_1) <- 1/diag(V.e)
+  mu.e <- z
+  
+  V.g.poumm <- try(chol2inv(chol(V.g_1 + V.e_1)), silent = TRUE)
+  
+  if(class(V.g.poumm)=='try-error') {
+    warning(V.g.poumm)
+    list(V.g = V.g, V.g_1 = V.g_1, mu.g = mu.g, V.e = V.e, V.e_1 = V.e_1, 
+         mu.e = mu.e)
+  } else {
+    mu.g.poumm <- V.g.poumm %*% (V.g_1 %*% mu.g + V.e_1 %*% mu.e)
+    
+    list(V.g = V.g, V.g_1 = V.g_1, mu.g = mu.g, V.e = V.e, V.e_1 = V.e_1, 
+         mu.e = mu.e, V.g.poumm = V.g.poumm, mu.g.poumm = mu.g.poumm)
+  }
+}
